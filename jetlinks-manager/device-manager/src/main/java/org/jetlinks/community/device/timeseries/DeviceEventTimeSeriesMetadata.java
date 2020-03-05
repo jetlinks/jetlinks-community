9113b2bd18c3a916eef8b7e9350efb64bddbdc8a
zhouhao <zh.sqy@qq.com>
2020-03-05 19:14:18 +0800

    优化类型判断

--- jetlinks-manager/device-manager/src/main/java/org/jetlinks/community/device/timeseries/DeviceEventTimeSeriesMetadata.java
+++ jetlinks-manager/device-manager/src/main/java/org/jetlinks/community/device/timeseries/DeviceEventTimeSeriesMetadata.java
@@ -1,5 +1,6 @@
 package org.jetlinks.community.device.timeseries;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.jetlinks.core.metadata.DataType;
 import org.jetlinks.core.metadata.EventMetadata;
 import org.jetlinks.core.metadata.PropertyMetadata;
@@ -48,7 +49,7 @@ class DeviceEventTimeSeriesMetadata implements TimeSeriesMetadata {
     public DeviceEventTimeSeriesMetadata(String productId, EventMetadata eventMetadata) {
         metric = DeviceTimeSeriesMetric.deviceEventMetric(productId, eventMetadata.getId());
         DataType type = eventMetadata.getType();
-        if (type instanceof ObjectType) {
+        if (CollectionUtils.isNotEmpty(((ObjectType) type).getProperties())) {
             metadata.addAll(((ObjectType) type).getProperties());
         } else {
             SimplePropertyMetadata property = new SimplePropertyMetadata();
