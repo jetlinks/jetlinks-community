5a81d815c0187c4896ef9ff5c3abe242bc9d6afb
zhou-hao <admin@hsweb.me>
2020-06-23 14:08:38 +0800

    增加版本信息

---
+++ jetlinks-components/common-component/src/main/java/org/jetlinks/community/Version.java
@@ -0,0 +1,13 @@
+package org.jetlinks.community;
+
+import lombok.Getter;
+
+@Getter
+public class Version {
+    public static Version current = new Version();
+
+    private final String edition = "community";
+
+    private final String version = "1.3.0-SNAPSHOT";
+
+}
