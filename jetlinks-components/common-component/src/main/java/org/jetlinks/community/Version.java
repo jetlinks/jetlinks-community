6a20531c69d4fabebd49d5034c33219562548d13
zhou-hao <admin@hsweb.me>
2020-07-23 10:09:33 +0800

    1.4.0-SNAPSHOT

--- jetlinks-components/common-component/src/main/java/org/jetlinks/community/Version.java
+++ jetlinks-components/common-component/src/main/java/org/jetlinks/community/Version.java
@@ -8,6 +8,6 @@ public class Version {
 
     private final String edition = "community";
 
-    private final String version = "1.3.0-SNAPSHOT";
+    private final String version = "1.4.0-SNAPSHOT";
 
 }
