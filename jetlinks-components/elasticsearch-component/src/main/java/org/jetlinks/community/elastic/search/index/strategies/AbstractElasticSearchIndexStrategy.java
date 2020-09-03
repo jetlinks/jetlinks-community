eeacd2217381c1bdaccd3c8f55c7ee8dc463aada
zhou-hao <admin@hsweb.me>
2020-09-03 18:54:04 +0800

    优化索引创建

--- jetlinks-components/elasticsearch-component/src/main/java/org/jetlinks/community/elastic/search/index/strategies/AbstractElasticSearchIndexStrategy.java
+++ jetlinks-components/elasticsearch-component/src/main/java/org/jetlinks/community/elastic/search/index/strategies/AbstractElasticSearchIndexStrategy.java
@@ -78,7 +78,7 @@ public abstract class AbstractElasticSearchIndexStrategy implements ElasticSearc
         Map<String, Object> mappingConfig = new HashMap<>();
         mappingConfig.put("properties", createElasticProperties(metadata.getProperties()));
         mappingConfig.put("dynamic_templates", createDynamicTemplates());
-        mappingConfig.forEach(request::mapping);
+        request.mapping("_doc",mappingConfig);
         return request;
     }
 
