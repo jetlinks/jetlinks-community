c728da0e4f8be7b3d1b0a6d5b833d1928dc539d9
zhou-hao <admin@hsweb.me>
2020-11-30 18:45:49 +0800

    触发子设备session注册事件

--- jetlinks-standalone/src/main/java/org/jetlinks/community/standalone/configuration/DefaultDeviceSessionManager.java
+++ jetlinks-standalone/src/main/java/org/jetlinks/community/standalone/configuration/DefaultDeviceSessionManager.java
@@ -227,7 +227,10 @@ public class DefaultDeviceSessionManager implements DeviceSessionManager {
                     .online(session.getServerId().orElse(serverId), session.getId(), session.getClientAddress().map(String::valueOf).orElse(null))
                     .then(deviceOperator.setConfig(DeviceConfigKey.parentGatewayId, deviceId))
                     .thenReturn(new ChildrenDeviceSession(childrenDeviceId, session, deviceOperator)))
-                .doOnNext(s -> children.computeIfAbsent(deviceId, __ -> new ConcurrentHashMap<>()).put(childrenDeviceId, s));
+                .doOnNext(s ->{
+                    registerListener.next(s);
+                    children.computeIfAbsent(deviceId, __ -> new ConcurrentHashMap<>()).put(childrenDeviceId, s);
+                });
         });
 
     }
