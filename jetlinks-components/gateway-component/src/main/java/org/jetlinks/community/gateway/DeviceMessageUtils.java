57d2f5fdc9de72ce9ed3d6ac806a02af812a0ab8
zhou-hao <admin@hsweb.me>
2021-03-11 14:30:11 +0800

    删除已经废弃的功能

--- jetlinks-components/gateway-component/src/main/java/org/jetlinks/community/gateway/DeviceMessageUtils.java
+++ jetlinks-components/gateway-component/src/main/java/org/jetlinks/community/gateway/DeviceMessageUtils.java
@@ -15,17 +15,6 @@ import java.util.Optional;
 
 public class DeviceMessageUtils {
 
-    @SuppressWarnings("all")
-    public static Optional<DeviceMessage> convert(TopicMessage message){
-        Object nativeMessage = message.convertMessage();
-        if (nativeMessage instanceof DeviceMessage) {
-            return Optional.of((DeviceMessage)nativeMessage);
-        } else if (nativeMessage instanceof Map) {
-            return MessageType.convertMessage(((Map<String, Object>) nativeMessage));
-        }
-        return Optional.empty();
-    }
-
     public static Optional<DeviceMessage> convert(TopicPayload message) {
         return Optional.of(message.decode(DeviceMessage.class));
     }
