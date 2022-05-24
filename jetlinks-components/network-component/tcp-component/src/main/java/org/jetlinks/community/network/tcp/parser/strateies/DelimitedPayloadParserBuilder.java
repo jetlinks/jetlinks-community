72e30119468c1b644c0049b7b52647c3079ecbfa
zhouhao <zh.sqy@qq.com>
2022-05-24 17:20:40 +0800

    优化

--- jetlinks-components/network-component/tcp-component/src/main/java/org/jetlinks/community/network/tcp/parser/strateies/DelimitedPayloadParserBuilder.java
+++ jetlinks-components/network-component/tcp-component/src/main/java/org/jetlinks/community/network/tcp/parser/strateies/DelimitedPayloadParserBuilder.java
@@ -1,7 +1,7 @@
 package org.jetlinks.community.network.tcp.parser.strateies;
 
 import io.vertx.core.parsetools.RecordParser;
-import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.jetlinks.community.ValueObject;
 import org.jetlinks.community.network.tcp.parser.PayloadParserType;
 
@@ -14,7 +14,9 @@ public class DelimitedPayloadParserBuilder extends VertxPayloadParserBuilder {
     @Override
     protected RecordParser createParser(ValueObject config) {
 
-        return RecordParser.newDelimited(StringEscapeUtils.unescapeJava(config.getString("delimited")
+        return RecordParser.newDelimited(StringEscapeUtils.unescapeJava(
+            config
+                .getString("delimited")
                 .orElseThrow(() -> new IllegalArgumentException("delimited can not be null"))));
     }
 
