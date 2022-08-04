31299b2e28dee46ee6c219af7ba0b7359ae27a2c
zhouhao <zh.sqy@qq.com>
2022-08-04 14:24:55 +0800

    增加api配置信息

--- jetlinks-standalone/src/main/java/org/jetlinks/community/standalone/web/SystemInfoController.java
+++ jetlinks-standalone/src/main/java/org/jetlinks/community/standalone/web/SystemInfoController.java
@@ -1,20 +1,32 @@
 package org.jetlinks.community.standalone.web;
 
+import lombok.AllArgsConstructor;
 import org.hswebframework.web.authorization.annotation.Authorize;
 import org.jetlinks.community.Version;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @RequestMapping("/system")
 @RestController
+@AllArgsConstructor
 public class SystemInfoController {
+
+    private final ApiInfoProperties apiInfoProperties;
 
     @GetMapping("/version")
     @Authorize(ignore = true)
     public Mono<Version> getVersion() {
         return Mono.just(Version.current);
+    }
+
+    @GetMapping("/apis")
+    @Authorize(ignore = true)
+    public Mono<ApiInfoProperties> getApis() {
+        return Mono.just(apiInfoProperties);
     }
 
 }
