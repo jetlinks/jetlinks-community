4c1de801ac8695b28c205613cf8f7018079cc7bf
zhouhao <zh.sqy@qq.com>
2022-12-13 18:01:52 +0800

    优化邮件通知

--- jetlinks-components/notify-component/notify-email/src/main/java/org/jetlinks/community/notify/email/embedded/EmailTemplate.java
+++ jetlinks-components/notify-component/notify-email/src/main/java/org/jetlinks/community/notify/email/embedded/EmailTemplate.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.jetlinks.community.notify.NotifyVariableBusinessConstant;
 import org.jetlinks.community.notify.template.AbstractTemplate;
 import org.jetlinks.community.notify.template.VariableDefinition;
+import org.jetlinks.community.utils.ConverterUtils;
 import org.jetlinks.core.metadata.types.ArrayType;
 import org.jetlinks.core.metadata.types.FileType;
 import org.jetlinks.community.notify.template.Variable;
@@ -18,9 +19,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.jetlinks.community.notify.email.embedded.EmailTemplate.Attachment.LOCATION_KEY;
+import static org.jetlinks.community.notify.email.embedded.EmailTemplate.Attachment.locationKey;
 
 @Getter
 @Setter
@@ -37,14 +40,11 @@ public class EmailTemplate extends AbstractTemplate<EmailTemplate> {
     private List<String> sendTo;
 
     public Mono<List<String>> getSendTo(Map<String, Object> context) {
-        return VariableSource.resolveValue(SEND_TO_KEY, context, RelationConstants.UserProperty.email)
-            .map(String::valueOf)
-            .collect(Collectors.joining(","))
-            .filter(StringUtils::isNoneBlank)
-            .map(value -> Arrays.stream(value
-                .split(","))
-                .collect(Collectors.toList()))
-            .switchIfEmpty(Mono.just(sendTo == null ? new ArrayList<>() : sendTo));
+        return VariableSource
+            .resolveValue(SEND_TO_KEY, context, RelationConstants.UserProperty.email)
+            .flatMapIterable(e -> ConverterUtils.convertToList(e, String::valueOf))
+            .concatWith(Mono.justOrEmpty(sendTo).flatMapIterable(Function.identity()))
+            .collectList();
     }
 
     @Getter
@@ -59,6 +59,10 @@ public class EmailTemplate extends AbstractTemplate<EmailTemplate> {
         private String name;
 
         private String location;
+
+        public static String locationKey(int index) {
+            return "_attach_location_" + index;
+        }
     }
 
 
@@ -73,7 +77,7 @@ public class EmailTemplate extends AbstractTemplate<EmailTemplate> {
                     .id(SEND_TO_KEY)
                     .name("收件人")
                     .expand(NotifyVariableBusinessConstant.businessId,
-                            NotifyVariableBusinessConstant.NotifyVariableBusinessTypes.userType)
+                        NotifyVariableBusinessConstant.NotifyVariableBusinessTypes.userType)
                     .required(true)
                     .type(ArrayType.ID)
                     .build()
@@ -82,21 +86,25 @@ public class EmailTemplate extends AbstractTemplate<EmailTemplate> {
 
         List<Attachment> attachments = getAttachments();
         if (!CollectionUtils.isEmpty(attachments)) {
-            variables.addAll(attachments
-                .stream()
-                //附件名称不为空，附件数据为空
-                .filter(attachment -> StringUtils.isNotEmpty(attachment.getName())
-                    && StringUtils.isEmpty(attachment.getLocation()))
-                .map(attachment -> VariableDefinition
-                    .builder()
-                    .id(LOCATION_KEY)
-                    .name(attachment.getName())
-                    .type(FileType.ID)
-                    .description(attachment.getName())
-                    .format(Variable.FileFormat.any)
-                    .required(true)
-                    .build())
-                .collect(Collectors.toList()));
+            int index = 0;
+            for (Attachment attachment : attachments) {
+                index++;
+                if (StringUtils.isNotEmpty(attachment.getName())
+                    && StringUtils.isEmpty(attachment.getLocation())) {
+                    continue;
+                }
+                variables.add(
+                    VariableDefinition
+                        .builder()
+                        .id(locationKey(index))
+                        .name(attachment.getName())
+                        .type(FileType.ID)
+                        .description(attachment.getName())
+                        .format(Variable.FileFormat.any)
+                        .required(true)
+                        .build()
+                );
+            }
         }
         return variables;
     }
