f8a3199138c7ffe78980a7583466802e9b567943
zhouhao <zh.sqy@qq.com>
2023-05-10 13:42:31 +0800

    fix(设备消息): 修复设备上线消息错误

--- jetlinks-manager/device-manager/src/main/java/org/jetlinks/community/device/message/DeviceMessageConnector.java
+++ jetlinks-manager/device-manager/src/main/java/org/jetlinks/community/device/message/DeviceMessageConnector.java
@@ -148,11 +148,11 @@ public class DeviceMessageConnector implements DecodedClientMessageHandler {
             }
             //从会话管理器里监听会话注册,转发为设备上线消息
             if (event.getType() == DeviceSessionEvent.Type.unregister) {
-                return handleSessionMessage(new DeviceOnlineMessage(),event.getSession());
+                return handleSessionMessage(new DeviceOfflineMessage(),event.getSession());
             }
             //从会话管理器里监听会话注销,转发为设备离线消息
             if (event.getType() == DeviceSessionEvent.Type.register) {
-                return handleSessionMessage(new DeviceOfflineMessage(),event.getSession());
+                return handleSessionMessage(new DeviceOnlineMessage(),event.getSession());
             }
             return Mono.empty();
         });
