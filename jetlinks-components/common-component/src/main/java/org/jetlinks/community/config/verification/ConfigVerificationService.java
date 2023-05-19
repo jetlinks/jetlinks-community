754e8a19d12c4422a0b632d59e661e2b3e6bc65a
ayan <bestfeng1020@163.com>
2023-05-19 11:58:37 +0800

    feat(系统配置): base-path值正确性校验

--- jetlinks-components/common-component/src/main/java/org/jetlinks/community/config/verification/ConfigVerificationService.java
+++ jetlinks-components/common-component/src/main/java/org/jetlinks/community/config/verification/ConfigVerificationService.java
@@ -1,12 +1,16 @@
 package org.jetlinks.community.config.verification;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.hswebframework.web.crud.events.EntitySavedEvent;
 import org.hswebframework.web.exception.BusinessException;
+import org.jetlinks.community.config.entity.ConfigEntity;
 import org.jetlinks.reactor.ql.utils.CastUtils;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.UnknownHostException;
@@ -36,6 +40,17 @@ public class ConfigVerificationService {
     public Mono<Void> basePathValidate(ServerWebExchange response) {
         response.getResponse().getHeaders().set("auth", PATH_VERIFICATION_URI);
         return Mono.empty();
+    }
+
+
+    @EventListener
+    public void handleConfigSavedEvent(EntitySavedEvent<ConfigEntity> event){
+        //base-path校验
+        event.async(
+            Flux.fromIterable(event.getEntity())
+                .filter(config -> Objects.equals(config.getScope(), "paths"))
+                .flatMap(config-> doBasePathValidate(config.getProperties().get("base-path")))
+        );
     }
 
 
--- jetlinks-components/common-component/src/main/java/org/jetlinks/community/config/web/SystemConfigManagerController.java
+++ jetlinks-components/common-component/src/main/java/org/jetlinks/community/config/web/SystemConfigManagerController.java
@@ -16,7 +16,6 @@ import org.jetlinks.community.ValueObject;
 import org.jetlinks.community.config.ConfigManager;
 import org.jetlinks.community.config.ConfigPropertyDef;
 import org.jetlinks.community.config.ConfigScope;
-import org.jetlinks.community.config.verification.ConfigVerificationService;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -25,7 +24,6 @@ import reactor.core.publisher.Mono;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 
 @RestController
@@ -36,8 +34,6 @@ import java.util.Objects;
 public class SystemConfigManagerController {
 
     private final ConfigManager configManager;
-
-    private final ConfigVerificationService configVerificationService;
 
     @GetMapping("/scopes")
     @QueryAction
@@ -104,15 +100,7 @@ public class SystemConfigManagerController {
     public Mono<Void> saveConfig(@RequestBody Flux<Scope> scope) {
 
         return scope
-            .flatMap(scopeConfig -> {
-                //base-path校验
-                if (Objects.equals(scopeConfig.getScope(), "paths")) {
-                    return configVerificationService
-                        .doBasePathValidate(scopeConfig.getProperties().get("base-path"))
-                        .then(configManager.setProperties(scopeConfig.getScope(), scopeConfig.getProperties()));
-                }
-                return configManager.setProperties(scopeConfig.getScope(), scopeConfig.getProperties());
-            })
+            .flatMap(scopeConfig -> configManager.setProperties(scopeConfig.getScope(), scopeConfig.getProperties()))
             .then();
     }
 
