6bf10c87a2145e2c55563a35478d28028fae38e9
ayan <bestfeng1020@163.com>
2023-08-24 10:02:13 +0800

    fix(docker): 规范社区办docker镜像命名

--- build-and-push-docker.sh
+++ build-and-push-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dockerImage=registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-standalone:$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+dockerImage=registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-community:$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 ./mvnw clean package -Dmaven.test.skip=true -Dmaven.build.timestamp="$(date "+%Y-%m-%d %H:%M:%S")"
 if [ $? -ne 0 ];then
     echo "构建失败!"
