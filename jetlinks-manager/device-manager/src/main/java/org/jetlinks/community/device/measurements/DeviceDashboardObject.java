156db917823b41da37876c8e50cea11975f39852
zhouhao <zh.sqy@qq.com>
2023-10-27 15:50:42 +0800

    fix: 修复在设备里单独定义物模型时,订阅的数据格式不对问题.

--- jetlinks-manager/device-manager/src/main/java/org/jetlinks/community/device/measurements/DeviceDashboardObject.java
+++ jetlinks-manager/device-manager/src/main/java/org/jetlinks/community/device/measurements/DeviceDashboardObject.java
@@ -1,10 +1,12 @@
 package org.jetlinks.community.device.measurements;
 
+import lombok.Generated;
 import org.jetlinks.community.dashboard.DashboardObject;
 import org.jetlinks.community.dashboard.Measurement;
 import org.jetlinks.community.dashboard.ObjectDefinition;
 import org.jetlinks.community.device.service.data.DeviceDataService;
 import org.jetlinks.core.device.DeviceProductOperator;
+import org.jetlinks.core.device.DeviceRegistry;
 import org.jetlinks.core.event.EventBus;
 import org.jetlinks.core.metadata.DeviceMetadata;
 import reactor.core.publisher.Flux;
@@ -21,33 +23,40 @@ public class DeviceDashboardObject implements DashboardObject {
 
     private final DeviceDataService deviceDataService;
 
+    private final DeviceRegistry registry;
+
     private DeviceDashboardObject(String id, String name,
                                   DeviceProductOperator productOperator,
                                   EventBus eventBus,
-                                  DeviceDataService dataService) {
+                                  DeviceDataService dataService,
+                                  DeviceRegistry registry) {
         this.id = id;
         this.name = name;
         this.productOperator = productOperator;
         this.eventBus = eventBus;
         this.deviceDataService = dataService;
+        this.registry = registry;
     }
 
     public static DeviceDashboardObject of(String id, String name,
-                                                                                DeviceProductOperator productOperator,
-                                                                                EventBus eventBus,
-                                                                                DeviceDataService dataService ) {
-        return new DeviceDashboardObject(id, name, productOperator, eventBus, dataService);
+                                           DeviceProductOperator productOperator,
+                                           EventBus eventBus,
+                                           DeviceDataService dataService,
+                                           DeviceRegistry registry) {
+        return new DeviceDashboardObject(id, name, productOperator, eventBus, dataService, registry);
     }
 
     @Override
     public ObjectDefinition getDefinition() {
         return new ObjectDefinition() {
             @Override
+            @Generated
             public String getId() {
                 return id;
             }
 
             @Override
+            @Generated
             public String getName() {
                 return name;
             }
@@ -56,40 +65,57 @@ public class DeviceDashboardObject implements DashboardObject {
 
     @Override
     public Flux<Measurement> getMeasurements() {
-        return Flux.concat(
+        return Flux
+            .concat(
 
-            productOperator.getMetadata()
-                .flatMapIterable(DeviceMetadata::getEvents)
-                .map(event -> new DeviceEventMeasurement(productOperator.getId(), eventBus, event, deviceDataService)),
+                productOperator
+                    .getMetadata()
+                    .flatMapIterable(DeviceMetadata::getEvents)
+                    .map(event -> new DeviceEventMeasurement(productOperator.getId(),
+                                                             eventBus,
+                                                             event,
+                                                             deviceDataService)),
 
-            productOperator.getMetadata()
-                .map(metadata -> new DevicePropertiesMeasurement(productOperator.getId(), eventBus, deviceDataService, metadata)),
+                Mono.just(new DevicePropertiesMeasurement(productOperator.getId(),
+                                                          eventBus,
+                                                          deviceDataService,
+                                                          registry)),
 
-            productOperator.getMetadata()
-                .map(metadata -> new DeviceEventsMeasurement(productOperator.getId(), eventBus, metadata, deviceDataService)),
+                productOperator
+                    .getMetadata()
+                    .map(metadata -> new DeviceEventsMeasurement(productOperator.getId(),
+                                                                 eventBus,
+                                                                 metadata,
+                                                                 deviceDataService)),
 
-            productOperator.getMetadata()
-                .flatMapIterable(DeviceMetadata::getProperties)
-                .map(event -> new DevicePropertyMeasurement(productOperator.getId(), eventBus, event, deviceDataService))
-        );
+                productOperator
+                    .getMetadata()
+                    .flatMapIterable(DeviceMetadata::getProperties)
+                    .map(event -> new DevicePropertyMeasurement(productOperator.getId(),
+                                                                eventBus,
+                                                                event,
+                                                                deviceDataService))
+            );
     }
 
     @Override
     public Mono<Measurement> getMeasurement(String id) {
         if ("properties".equals(id)) {
-            return productOperator.getMetadata()
-                .map(metadata -> new DevicePropertiesMeasurement(productOperator.getId(), eventBus, deviceDataService, metadata));
+            return Mono.just(new DevicePropertiesMeasurement(productOperator.getId(), eventBus, deviceDataService, registry));
         }
         if ("events".equals(id)) {
-            return productOperator.getMetadata()
+            return productOperator
+                .getMetadata()
                 .map(metadata -> new DeviceEventsMeasurement(productOperator.getId(), eventBus, metadata, deviceDataService));
         }
-        return productOperator.getMetadata()
+        return productOperator
+            .getMetadata()
             .flatMap(metadata -> Mono.justOrEmpty(metadata.getEvent(id)))
             .<Measurement>map(event -> new DeviceEventMeasurement(productOperator.getId(), eventBus, event, deviceDataService))
             //事件没获取到则尝试获取属性
-            .switchIfEmpty(productOperator.getMetadata()
-                .flatMap(metadata -> Mono.justOrEmpty(metadata.getProperty(id)))
-                .map(event -> new DevicePropertyMeasurement(productOperator.getId(), eventBus, event, deviceDataService)));
+            .switchIfEmpty(productOperator
+                               .getMetadata()
+                               .flatMap(metadata -> Mono.justOrEmpty(metadata.getProperty(id)))
+                               .map(event -> new DevicePropertyMeasurement(productOperator.getId(), eventBus, event, deviceDataService)));
     }
 }
