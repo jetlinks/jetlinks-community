acf38bc7e14c3c8bbd2612795863a916233611b7
zhouhao <zh.sqy@qq.com>
2023-11-28 17:02:34 +0800

    refactor: 优化规则日志记录逻辑

--- jetlinks-components/rule-engine-component/src/main/java/org/jetlinks/community/rule/engine/event/handler/RuleLogHandler.java
+++ jetlinks-components/rule-engine-component/src/main/java/org/jetlinks/community/rule/engine/event/handler/RuleLogHandler.java
@@ -1,6 +1,8 @@
 package org.jetlinks.community.rule.engine.event.handler;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.hswebframework.web.bean.FastBeanCopier;
 import org.jetlinks.community.elastic.search.service.ElasticSearchService;
 import org.jetlinks.community.gateway.annotation.Subscribe;
 import org.jetlinks.community.rule.engine.entity.RuleEngineExecuteEventInfo;
@@ -19,14 +21,15 @@ public class RuleLogHandler {
     @Autowired
     private ElasticSearchService elasticSearchService;
 
-    @Subscribe("/rule-engine/*/*/event/*")
+    @Subscribe("/rule-engine/*/*/event/${rule.engine.event.level:error}")
     public Mono<Void> handleEvent(TopicPayload event) {
-
         return elasticSearchService.commit(RuleEngineLoggerIndexProvider.RULE_EVENT_LOG, RuleEngineExecuteEventInfo.of(event));
     }
 
-    @Subscribe("/rule-engine/*/*/logger/*")
+    @Subscribe("/rule-engine/*/*/logger/${rule.engine.logging.level:info,warn,error}")
     public Mono<Void> handleLog(LogEvent event) {
-        return elasticSearchService.commit(RuleEngineLoggerIndexProvider.RULE_LOG, event);
+        JSONObject jsonObject = FastBeanCopier.copy(event, new JSONObject());
+        jsonObject.put("createTime", System.currentTimeMillis());
+        return elasticSearchService.commit(RuleEngineLoggerIndexProvider.RULE_LOG, jsonObject);
     }
 }
