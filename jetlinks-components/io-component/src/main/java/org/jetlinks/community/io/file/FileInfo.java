4a4be8c449186019af0ce180ef524b30c9ab9050
zhouhao <zh.sqy@qq.com>
2024-04-07 14:59:50 +0800

    refactor: 优化文件地址逻辑

--- jetlinks-components/io-component/src/main/java/org/jetlinks/community/io/file/FileInfo.java
+++ jetlinks-components/io-component/src/main/java/org/jetlinks/community/io/file/FileInfo.java
@@ -3,6 +3,7 @@ package org.jetlinks.community.io.file;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.io.FilenameUtils;
+import org.jetlinks.community.io.utils.FileUtils;
 import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 
@@ -42,48 +43,27 @@ public class FileInfo {
         if (!apiBashPath.endsWith("/")) {
             apiBashPath = apiBashPath + "/";
         }
-        accessUrl = apiBashPath + "file/" + id + "?accessKey=" + accessKey().orElse("");
+        accessUrl = apiBashPath + "file/" + id + "." + extension + "?accessKey=" + accessKey().orElse("");
     }
 
 
     public MediaType mediaType() {
-        if (!StringUtils.hasText(extension)) {
-            return MediaType.APPLICATION_OCTET_STREAM;
-        }
-        switch (extension.toLowerCase()) {
-            case "jpg":
-            case "jpeg":
-                return MediaType.IMAGE_JPEG;
-            case "png":
-                return MediaType.IMAGE_PNG;
-            case "gif":
-                return MediaType.IMAGE_GIF;
-            case "mp4":
-                return MediaType.parseMediaType("video/mp4");
-            case "flv":
-                return MediaType.parseMediaType("video/x-flv");
-            case "text":
-            case "txt":
-                return MediaType.TEXT_PLAIN;
-            case "js":
-                return MediaType.APPLICATION_JSON;
-            default:
-                return MediaType.APPLICATION_OCTET_STREAM;
-        }
+        return FileUtils.getMediaTypeByExtension(extension);
     }
 
 
-    public boolean hasOption(FileOption option){
-        if(options==null){
+    public boolean hasOption(FileOption option) {
+        if (options == null) {
             return false;
         }
         for (FileOption fileOption : options) {
-            if(fileOption==option){
+            if (fileOption == option) {
                 return true;
             }
         }
         return false;
     }
+
     public FileInfo withFileName(String fileName) {
         name = fileName;
         extension = FilenameUtils.getExtension(fileName);
