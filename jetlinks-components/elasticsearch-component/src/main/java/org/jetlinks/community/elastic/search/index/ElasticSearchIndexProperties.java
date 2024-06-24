6a3901f37019d1f38510d07a544409cbdc1fd951
PengyuDeng <89559616+PengyuDeng@users.noreply.github.com>
2024-06-24 10:27:15 +0800

    refactor(Es): es索引配置增加拓展性。

--- jetlinks-components/elasticsearch-component/src/main/java/org/jetlinks/community/elastic/search/index/ElasticSearchIndexProperties.java
+++ jetlinks-components/elasticsearch-component/src/main/java/org/jetlinks/community/elastic/search/index/ElasticSearchIndexProperties.java
@@ -4,6 +4,10 @@ import lombok.*;
 import org.elasticsearch.common.settings.Settings;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 @Getter
 @Setter
 @Builder
@@ -16,11 +20,14 @@ public class ElasticSearchIndexProperties {
 
     private int numberOfReplicas = 0;
 
+    private Map<String,String> options = new HashMap<>();
+
     public Settings toSettings() {
 
         return Settings.builder()
             .put("number_of_shards", Math.max(1, numberOfShards))
             .put("number_of_replicas", numberOfReplicas)
+            .putProperties(options, Function.identity())
             .build();
     }
 }
