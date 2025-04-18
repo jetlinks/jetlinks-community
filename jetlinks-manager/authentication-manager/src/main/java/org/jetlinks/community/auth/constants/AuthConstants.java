8eb93cb82f2515e03d1412a7b477b40ff4a239c0
fighter-wang <11291691+fighter-wang@user.noreply.gitee.com>
2025-04-18 16:38:50 +0800

    fix(auth): 修复用户绑定组织，查询用户关联组织失败的问题

---
+++ jetlinks-manager/authentication-manager/src/main/java/org/jetlinks/community/auth/constants/AuthConstants.java
@@ -0,0 +1,14 @@
+package org.jetlinks.community.auth.constants;
+
+/**
+ * @author wangsheng
+ */
+public interface AuthConstants {
+
+    /**
+     * 标识是否为直接关联的维度,如直属部门
+     *
+     * @see org.hswebframework.web.authorization.Dimension#getOption(String)
+     */
+    public static String IS_DIRECT = "direct";
+}
