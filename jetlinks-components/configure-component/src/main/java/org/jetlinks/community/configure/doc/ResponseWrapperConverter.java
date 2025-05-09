87369fa87b13cc30c48b759ca3228fea5314a3cb
PengyuDeng <89559616+PengyuDeng@users.noreply.github.com>
2025-05-09 09:34:33 +0800

    增加单独的类处理

---
+++ jetlinks-components/configure-component/src/main/java/org/jetlinks/community/configure/doc/ResponseWrapperConverter.java
@@ -0,0 +1,94 @@
+package org.jetlinks.community.configure.doc;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import io.swagger.v3.core.converter.AnnotatedType;
+import io.swagger.v3.core.converter.ModelConverter;
+import io.swagger.v3.core.converter.ModelConverterContext;
+import io.swagger.v3.oas.models.media.Schema;
+import org.hswebframework.web.api.crud.entity.EntityFactory;
+import org.hswebframework.web.crud.web.ResponseMessage;
+import org.reactivestreams.Publisher;
+import org.springdoc.core.converters.ResponseSupportConverter;
+import org.springdoc.core.providers.ObjectMapperProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.springdoc.core.converters.ConverterUtils.isResponseTypeToIgnore;
+
+@Component
+public class ResponseWrapperConverter extends ResponseSupportConverter {
+
+    private final ObjectMapperProvider mapperProvider;
+
+    private final EntityFactory entityFactory;
+
+    public ResponseWrapperConverter(ObjectMapperProvider mapperProvider,
+                                    EntityFactory entityFactory) {
+        super(mapperProvider);
+        this.mapperProvider = mapperProvider;
+        this.entityFactory = entityFactory;
+    }
+
+    @Override
+    public Schema<?> resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
+        JavaType javaType = mapperProvider.jsonMapper().constructType(type.getType());
+        if (javaType != null) {
+            Class<?> rawClass = entityFactory.getInstanceType(javaType.getRawClass());
+            if (rawClass == null) {
+                rawClass = javaType.getRawClass();
+            }
+            if (rawClass != null && Publisher.class.isAssignableFrom(rawClass)) {
+                JavaType actualType = javaType.containedType(0);
+                if (actualType != null) {
+                    if (actualType.getRawClass() == ResponseEntity.class ||
+                        actualType.getRawClass() == ResponseMessage.class) {
+                        return super.resolve(type, context, chain);
+                    }
+                    JavaType wrappedType = buildWrappedType(actualType, rawClass);
+                    return resolveWrappedType(context, chain, type, wrappedType);
+                }
+            } else if (isResponseTypeToIgnore(rawClass)) {
+                return null;
+            }
+        }
+        return continueResolve(type, context, chain);
+    }
+
+    private JavaType buildWrappedType(JavaType actualType, Class<?> rawClass) {
+        TypeFactory tf = mapperProvider.jsonMapper().getTypeFactory();
+        boolean isFlux = Flux.class.isAssignableFrom(rawClass);
+        try {
+            JavaType baseType = isFlux ? tf.constructCollectionType(List.class, actualType) : actualType;
+            JavaType responseMessageType = tf.constructParametricType(entityFactory.getInstanceType(ResponseMessage.class), baseType);
+            return tf.constructParametricType(rawClass, responseMessageType);
+        } catch (IllegalArgumentException ignore) {
+            return null;
+        }
+    }
+
+    private Schema<?> resolveWrappedType(ModelConverterContext context,
+                                         Iterator<ModelConverter> chain,
+                                         AnnotatedType originalType,
+                                         JavaType wrappedType) {
+        if (wrappedType == null) {
+            return continueResolve(originalType, context, chain);
+        }
+
+        return context.resolve(new AnnotatedType()
+                                   .type(wrappedType)
+                                   .jsonViewAnnotation(originalType.getJsonViewAnnotation())
+                                   .ctxAnnotations(originalType.getCtxAnnotations())
+                                   .resolveAsRef(true));
+    }
+
+    private Schema<?> continueResolve(AnnotatedType type,
+                                      ModelConverterContext context,
+                                      Iterator<ModelConverter> chain) {
+        return (chain.hasNext()) ? chain.next().resolve(type, context, chain) : null;
+    }
+}
--- jetlinks-components/configure-component/src/main/java/org/jetlinks/community/configure/doc/SpringDocCustomizerConfiguration.java
+++ jetlinks-components/configure-component/src/main/java/org/jetlinks/community/configure/doc/SpringDocCustomizerConfiguration.java
@@ -1,29 +1,15 @@
 package org.jetlinks.community.configure.doc;
 
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import io.swagger.v3.core.converter.AnnotatedType;
-import io.swagger.v3.core.converter.ModelConverter;
-import io.swagger.v3.core.converter.ModelConverterContext;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.media.Schema;
-import org.hswebframework.web.crud.web.ResponseMessage;
-import org.reactivestreams.Publisher;
+import org.hswebframework.web.api.crud.entity.EntityFactory;
 import org.springdoc.core.converters.ResponseSupportConverter;
 import org.springdoc.core.providers.ObjectMapperProvider;
 import org.springdoc.webflux.core.configuration.SpringDocWebFluxConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import reactor.core.publisher.Flux;
-
-import java.util.Iterator;
-import java.util.List;
-
-import static org.springdoc.core.converters.ConverterUtils.isResponseTypeToIgnore;
 
 @Configuration
 @AutoConfigureBefore(SpringDocWebFluxConfiguration.class)
@@ -39,65 +25,9 @@ public class SpringDocCustomizerConfiguration {
     }
 
     @Bean
-    public ResponseSupportConverter responseSupportConverter(ObjectMapperProvider springDocObjectMapper) {
-        return new ResponseSupportConverter(springDocObjectMapper) {
-            @Override
-            public Schema resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
-                JavaType javaType = springDocObjectMapper.jsonMapper().constructType(type.getType());
-                if (javaType != null) {
-                    Class<?> rawClass = javaType.getRawClass();
-                    if (rawClass != null && Publisher.class.isAssignableFrom(rawClass)) {
-                        JavaType actualType = javaType.containedType(0);
-                        if (actualType != null) {
-                            if (actualType.getRawClass() == ResponseEntity.class ||
-                                actualType.getRawClass() == ResponseMessage.class) {
-                                return super.resolve(type, context, chain);
-                            }
-                            JavaType wrappedType = buildWrappedType(actualType, rawClass);
-                            return resolveWrappedType(context, chain, type, wrappedType);
-                        }
-                    } else if (isResponseTypeToIgnore(rawClass)) {
-                        return null;
-                    }
-                }
-                return continueResolve(type, context, chain);
-            }
-
-            private JavaType buildWrappedType(JavaType actualType, Class<?> rawClass) {
-                TypeFactory tf = springDocObjectMapper.jsonMapper().getTypeFactory();
-                boolean isFlux = Flux.class.isAssignableFrom(rawClass);
-                try {
-                    JavaType baseType = isFlux ? tf.constructCollectionType(List.class, actualType) : actualType;
-                    JavaType responseMessageType = tf.constructParametricType(ResponseMessage.class, baseType);
-                    return tf.constructParametricType(rawClass, responseMessageType);
-                } catch (IllegalArgumentException ignore) {
-                    return null;
-                }
-            }
-
-            private Schema resolveWrappedType(ModelConverterContext context,
-                                              Iterator<ModelConverter> chain,
-                                              AnnotatedType originalType,
-                                              JavaType wrappedType) {
-                if (wrappedType == null) {
-                    return continueResolve(originalType, context, chain);
-                }
-
-                AnnotatedType newType = new AnnotatedType()
-                    .type(wrappedType)
-                    .jsonViewAnnotation(originalType.getJsonViewAnnotation())
-                    .ctxAnnotations(originalType.getCtxAnnotations())
-                    .resolveAsRef(true);
-
-                return context.resolve(newType);
-            }
-
-            private Schema continueResolve(AnnotatedType type,
-                                           ModelConverterContext context,
-                                           Iterator<ModelConverter> chain) {
-                return (chain.hasNext()) ? chain.next().resolve(type, context, chain) : null;
-            }
-        };
+    public ResponseSupportConverter responseSupportConverter(ObjectMapperProvider springDocObjectMapper,
+                                                             EntityFactory entityFactory) {
+        return new ResponseWrapperConverter(springDocObjectMapper, entityFactory);
     }
 
 }
