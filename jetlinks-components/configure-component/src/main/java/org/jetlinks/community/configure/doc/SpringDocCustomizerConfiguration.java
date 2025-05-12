8ad6ff46749c5c6bf3b7529b6e734a79ca911e04
PengyuDeng <89559616+PengyuDeng@users.noreply.github.com>
2025-05-12 15:05:48 +0800

    openai参数配置化

--- jetlinks-components/configure-component/src/main/java/org/jetlinks/community/configure/doc/SpringDocCustomizerConfiguration.java
+++ jetlinks-components/configure-component/src/main/java/org/jetlinks/community/configure/doc/SpringDocCustomizerConfiguration.java
@@ -1,8 +1,5 @@
 package org.jetlinks.community.configure.doc;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
 import org.hswebframework.web.api.crud.entity.EntityFactory;
 import org.springdoc.core.converters.ResponseSupportConverter;
 import org.springdoc.core.providers.ObjectMapperProvider;
@@ -14,15 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @AutoConfigureBefore(SpringDocWebFluxConfiguration.class)
 public class SpringDocCustomizerConfiguration {
-
-    @Bean
-    public OpenAPI springDocCustomizer() {
-        return new OpenAPI()
-            .info(new Info().title("jetlinks")
-                            .contact(new Contact())
-                            .description("jetlinks平台API")
-                            .version("2.10"));
-    }
 
     @Bean
     public ResponseSupportConverter responseSupportConverter(EntityFactory entityFactory,
