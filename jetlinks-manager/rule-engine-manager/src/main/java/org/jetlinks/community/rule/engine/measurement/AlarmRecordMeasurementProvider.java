317fc42c6b935af119ccb9055f109371be38ea50
zhouhao <zh.sqy@qq.com>
2025-06-24 11:02:22 +0800

    fix: 修复告警记录统计错误

--- jetlinks-manager/rule-engine-manager/src/main/java/org/jetlinks/community/rule/engine/measurement/AlarmRecordMeasurementProvider.java
+++ jetlinks-manager/rule-engine-manager/src/main/java/org/jetlinks/community/rule/engine/measurement/AlarmRecordMeasurementProvider.java
@@ -1,33 +1,8 @@
-/*
- * Copyright 2025 JetLinks https://www.jetlinks.cn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.jetlinks.community.rule.engine.measurement;
 
-import com.google.common.collect.Maps;
-import io.micrometer.core.instrument.MeterRegistry;
-import org.jetlinks.community.PropertyConstants;
 import org.jetlinks.community.dashboard.supports.StaticMeasurementProvider;
-import org.jetlinks.community.micrometer.MeterRegistryManager;
-import org.jetlinks.community.rule.engine.alarm.AlarmConstants;
-import org.jetlinks.community.rule.engine.entity.AlarmHistoryInfo;
-import org.jetlinks.community.timeseries.TimeSeriesManager;
-import org.jetlinks.community.utils.ConverterUtils;
-import org.springframework.context.event.EventListener;
+import org.jetlinks.community.rule.engine.service.AlarmHistoryService;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 /**
  * @author bestfeng
@@ -35,36 +10,24 @@ import java.util.Map;
 @Component
 public class AlarmRecordMeasurementProvider extends StaticMeasurementProvider {
 
-    MeterRegistry registry;
+//    MeterRegistry registry;
 
-    public AlarmRecordMeasurementProvider(MeterRegistryManager registryManager,
-                                          TimeSeriesManager timeSeriesManager) {
+    public AlarmRecordMeasurementProvider(AlarmHistoryService historyService) {
         super(AlarmDashboardDefinition.alarm, AlarmObjectDefinition.record);
 
-        registry = registryManager.getMeterRegister(AlarmTimeSeriesMetric.alarmStreamMetrics().getId());
-        addMeasurement(new AlarmRecordTrendMeasurement(timeSeriesManager));
-        addMeasurement(new AlarmRecordRankMeasurement(timeSeriesManager));
+//        registry = registryManager.getMeterRegister(AlarmTimeSeriesMetric.alarmStreamMetrics().getId());
+        addMeasurement(new AlarmRecordTrendMeasurement(historyService));
+        addMeasurement(new AlarmRecordRankMeasurement(historyService));
 
     }
 
-    @EventListener
-    public void aggAlarmRecord(AlarmHistoryInfo info) {
-        registry
-            .counter("record-agg", getTags(info))
-            .increment();
-    }
+//    @EventListener
+//    public void aggAlarmRecord(AlarmHistoryInfo info) {
+//        registry
+//            .counter("record-agg", getTags(info))
+//            .increment();
+//    }
 
 
 
-    public String[] getTags(AlarmHistoryInfo info) {
-        Map<String, Object> tagMap = Maps.newLinkedHashMap();
-
-      //  tagMap.put(AlarmConstants.ConfigKey.targetId, info.getTargetId());
-        //只需要记录targetType,用于统计 设备，产品等告警数量.
-        tagMap.put(AlarmConstants.ConfigKey.targetType, info.getTargetType());
-        //tagMap.put(AlarmConstants.ConfigKey.targetName, info.getTargetName());
-//        tagMap.put(AlarmConstants.ConfigKey.alarmConfigId, info.getAlarmConfigId());
-        tagMap.put(PropertyConstants.creatorId.getKey(), info.getCreatorId());
-        return ConverterUtils.convertMapToTags(tagMap);
-    }
 }
--- jetlinks-manager/rule-engine-manager/src/main/java/org/jetlinks/community/rule/engine/measurement/AlarmRecordRankMeasurement.java
+++ jetlinks-manager/rule-engine-manager/src/main/java/org/jetlinks/community/rule/engine/measurement/AlarmRecordRankMeasurement.java
@@ -1,36 +1,20 @@
-/*
- * Copyright 2025 JetLinks https://www.jetlinks.cn
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.jetlinks.community.rule.engine.measurement;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.jetlinks.community.dashboard.*;
 import org.jetlinks.community.dashboard.supports.StaticMeasurement;
-import org.jetlinks.community.timeseries.TimeSeriesManager;
-import org.jetlinks.community.timeseries.query.Aggregation;
-import org.jetlinks.community.timeseries.query.AggregationData;
-import org.jetlinks.community.timeseries.query.AggregationQueryParam;
 import org.jetlinks.core.metadata.ConfigMetadata;
 import org.jetlinks.core.metadata.DataType;
 import org.jetlinks.core.metadata.DefaultConfigMetadata;
 import org.jetlinks.core.metadata.types.IntType;
 import org.jetlinks.core.metadata.types.StringType;
-import org.springframework.util.StringUtils;
+import org.jetlinks.community.dashboard.*;
+import org.jetlinks.community.rule.engine.service.AlarmHistoryService;
+import org.jetlinks.community.timeseries.query.Aggregation;
+import org.jetlinks.community.timeseries.query.AggregationData;
+import org.jetlinks.community.timeseries.query.AggregationQueryParam;
 import reactor.core.publisher.Flux;
 
 import java.time.LocalDateTime;
@@ -44,22 +28,22 @@ import java.util.Objects;
  */
 public class AlarmRecordRankMeasurement extends StaticMeasurement {
 
-    TimeSeriesManager timeSeriesManager;
+    AlarmHistoryService historyService;
 
-    public AlarmRecordRankMeasurement(TimeSeriesManager timeSeriesManager) {
+    public AlarmRecordRankMeasurement(AlarmHistoryService historyService) {
         super(MeasurementDefinition.of("rank", "告警记录排名"));
-        this.timeSeriesManager = timeSeriesManager;
+        this.historyService = historyService;
         addDimension(new AggRecordRankDimension());
     }
 
 
     static ConfigMetadata aggConfigMetadata = new DefaultConfigMetadata()
-        .add("time", "周期", "例如: 1h,10m,30s", StringType.GLOBAL)
-        .add("agg", "聚合类型", "count,sum,avg,max,min", StringType.GLOBAL)
-        .add("format", "时间格式", "如: MM-dd:HH", StringType.GLOBAL)
-        .add("limit", "最大数据量", "", StringType.GLOBAL)
-        .add("from", "时间从", "", StringType.GLOBAL)
-        .add("to", "时间至", "", StringType.GLOBAL);
+            .add("time", "周期", "例如: 1h,10m,30s", StringType.GLOBAL)
+            .add("agg", "聚合类型", "count,sum,avg,max,min", StringType.GLOBAL)
+            .add("format", "时间格式", "如: MM-dd:HH", StringType.GLOBAL)
+            .add("limit", "最大数据量", "", StringType.GLOBAL)
+            .add("from", "时间从", "", StringType.GLOBAL)
+            .add("to", "时间至", "", StringType.GLOBAL);
 
 
     class AggRecordRankDimension implements MeasurementDimension {
@@ -87,49 +71,52 @@ public class AlarmRecordRankMeasurement extends StaticMeasurement {
         public AggregationQueryParam createQueryParam(MeasurementParameter parameter) {
             AggregationQueryParam aggregationQueryParam = AggregationQueryParam.of();
             aggregationQueryParam.setTimeProperty("alarmTime");
-            String targetType = parameter.getString("targetType").orElse(null);
-            String targetId = parameter.getString("targetId").orElse(null);
+
             return aggregationQueryParam
-                .groupBy(parameter.getString("group", "targetId"))
-                .count("targetId", "count")
-                .agg("targetId", Aggregation.TOP)
-                .agg("targetName", Aggregation.TOP)
-                .filter(query -> query
-                    .when(StringUtils.hasText(targetType), q -> q.and("targetType",targetType))
-                    .when(StringUtils.hasText(targetId), q -> q.and("targetId",targetId))
-                )
-                .limit(parameter.getInt("limit").orElse(1))
-                .from(parameter
-                          .getDate("from")
-                          .orElseGet(() -> Date
-                              .from(LocalDateTime
-                                        .now()
-                                        .plusDays(-1)
-                                        .atZone(ZoneId.systemDefault())
-                                        .toInstant())))
-                .to(parameter.getDate("to").orElse(new Date()));
+                    .groupBy(parameter.getString("group", "targetId"))
+                    .count("targetId", "count")
+                    .agg("targetId", Aggregation.TOP)
+                    .agg("targetName", Aggregation.TOP)
+                    .limit(parameter.getInt("limit").orElse(1))
+                    .from(parameter
+                                  .getDate("from")
+                                  .orElseGet(() -> Date
+                                          .from(LocalDateTime
+                                                        .now()
+                                                        .plusDays(-1)
+                                                        .atZone(ZoneId.systemDefault())
+                                                        .toInstant())))
+                    .to(parameter.getDate("to").orElse(new Date()));
         }
 
         @Override
         public Flux<SimpleMeasurementValue> getValue(MeasurementParameter parameter) {
 
             Comparator<AggregationData> comparator;
-            if (Objects.equals(parameter.getString("order",""), "asc")){
-                 comparator = Comparator.comparingLong(d-> d.getLong("count", 0L));
-            }else {
-                comparator = Comparator.<AggregationData>comparingLong(d-> d.getLong("count", 0L)).reversed();
+            if (Objects.equals(parameter.getString("order", ""), "asc")) {
+                comparator = Comparator.comparingLong(d -> d.getLong("count", 0L));
+            } else {
+                comparator = Comparator.<AggregationData>comparingLong(d -> d.getLong("count", 0L)).reversed();
             }
 
             AggregationQueryParam param = createQueryParam(parameter);
+            return param
+                    .execute(historyService::aggregation)
+                    .groupBy(a -> a.getString("targetId", null))
+                    .flatMap(fluxGroup -> fluxGroup.reduce(AggregationData::merge))
+                    .sort(comparator)
+                    .map(data -> SimpleMeasurementValue.of(new SimpleResult(data), 0))
+                    .take(param.getLimit());
+        }
 
-            return Flux.defer(() -> param
-                .execute(timeSeriesManager.getService(AlarmTimeSeriesMetric.alarmStreamMetrics())::aggregation)
-                .groupBy(a -> a.getString("targetId", null))
-                .flatMap(fluxGroup -> fluxGroup.reduce(AggregationData::merge))
-                .sort(comparator)
-                .map(data -> SimpleMeasurementValue.of(new SimpleResult(data), 0))
-            )
-                .take(param.getLimit());
+        public Flux<SimpleMeasurementValue> getValue(AggregationQueryParam param, Comparator<AggregationData> comparator) {
+            return param
+                    .execute(historyService::aggregation)
+                    .groupBy(a -> a.getString("targetId", null))
+                    .flatMap(fluxGroup -> fluxGroup.reduce(AggregationData::merge))
+                    .sort(comparator)
+                    .map(data -> SimpleMeasurementValue.of(new SimpleResult(data), 0))
+                    .take(param.getLimit());
         }
 
         @Getter
