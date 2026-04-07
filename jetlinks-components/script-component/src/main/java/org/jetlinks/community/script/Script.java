c4213b76121b3b9ea7bfc5e4e8f1aea780807293
zhouhao <zh.sqy@qq.com>
2026-04-07 14:31:10 +0800

    doc: update license header

--- jetlinks-components/script-component/src/main/java/org/jetlinks/community/script/Script.java
+++ jetlinks-components/script-component/src/main/java/org/jetlinks/community/script/Script.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 JetLinks https://www.jetlinks.cn
+ * Copyright 2026 JetLinks https://www.jetlinks.cn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.jetlinks.community.script;
 
 import lombok.*;
